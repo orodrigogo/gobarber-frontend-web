e5adfb2b21200fde890e67cea5e6193b14c44ec7
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-04 14:52:20 -0300

    feat: Estilização da autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from "react";
-
-// import { Container } from './styles';
+import { Link } from "react-router-dom";
+import logo from "~/assets/logo.svg";
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
